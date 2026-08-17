cac9e515bcc57d582f59f0abd0a974e2216fdea1
Dawson Alladin <81022330+dawgskieee@users.noreply.github.com>
2026-08-17 10:25:44 +0800

    updated

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["http://10.255.103.16:3000"],
-  output: 'export'
+  output: 'export',
+    images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
