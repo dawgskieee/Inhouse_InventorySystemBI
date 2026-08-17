bb538b0a4fcf20f3394cdca2735f211178becdc7
Dawson Alladin <81022330+dawgskieee@users.noreply.github.com>
2026-08-17 10:38:38 +0800

    remove output

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["http://10.255.103.16:3000"],
-  output: 'export',
+
     images: {
     unoptimized: true,
   },
