68b54a47056e93d8fbd42e281701beaa1ccdfce0
klarnaive <klarenzenaive09@gmail.com>
2026-09-25 15:49:39 +0800

    UP

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,14 +1,21 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    (typeof window !== "undefined" ? window.location.origin : "http://localhost:5000"),
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  config.headers = config.headers || {};
+
+  if (typeof window !== "undefined") {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
+
   return config;
 });
 
